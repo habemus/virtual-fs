08046dce5ec3019debbadf75286f77512e4d2380
Simon <simonxd@gmail.com>
2016-07-11 16:25:05 -0300

    removed chokidar and watching from hfs core

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const HFs = require('./lib');
 const vroot = require('vroot');
 
-module.exports = function (rootPath) {
-  var vfs = vroot(rootPath);
+module.exports = function (rootPath, options) {
+  var vfs = vroot(rootPath, options);
 
   return new HFs(vfs);
 };
